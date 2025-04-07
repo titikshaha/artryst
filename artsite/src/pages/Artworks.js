22784c8781fbfb98e9c41ed1af8913a68a4f146d
Titiksha <titiksha5248@gmail.com>
2025-04-07 23:46:58 +0530

    changes

--- artsite/src/pages/Artworks.js
+++ artsite/src/pages/Artworks.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import '../css/index2.css'
+import '../css/index2.css';
 import Portraits from '../components/artworks/Portraits';
 import Sketches from '../components/artworks/Sketches';
 import Everything from '../components/artworks/Everything';
 import Scenic from '../components/artworks/Scenic';
+
 function Artworks() {
-  const [setArtworks] = useState([]);
+  const [artworks, setArtworks] = useState([]);
 
   useEffect(() => {
     axios.get('/api/artworks')
@@ -18,30 +19,27 @@ function Artworks() {
       });
 
     window.scrollTo(0, 0);
-  }, []); // It's safe to leave setArtworks out
+  }, []);
 
   return (
     <div className="Arts">
-        <div className="section portrait">
-          <Portraits />
-        </div>
-        
-        <div className="section sketches">
-          <Sketches />
-        </div>
-
-        <div className = "section everything">
-          <Everything />
-        </div>
-  
-        <div className = "section scenic">
-          <Scenic />
-        </div>
-  
+      <div className="section portrait">
+        <Portraits artworks={artworks} />
       </div>
-    );
-  }
-  
 
+      <div className="section sketches">
+        <Sketches artworks={artworks} />
+      </div>
+
+      <div className="section everything">
+        <Everything artworks={artworks} />
+      </div>
+
+      <div className="section scenic">
+        <Scenic artworks={artworks} />
+      </div>
+    </div>
+  );
+}
 
 export default Artworks;
